b19c15873901865b153b49ea4d9a47652ff27427
Tony M <t.miba95@gmail.com>
2024-03-14 13:39:29 +0300

    Update Cli, Engine

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -8,7 +8,7 @@ public class Cli {
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String userName1 = scanner1.next();
-        System.out.println("Hello " + userName1 + "!");
+        System.out.println("Hello, " + userName1 + "!");
         scanner1.close();
     }
 }
