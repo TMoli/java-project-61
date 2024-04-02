d291cb57e59e6f0941640900ca9cd90693e1c2ef
Tony M <t.miba95@gmail.com>
2024-04-02 16:13:34 +0300

    Update Progression

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -16,12 +16,14 @@ public class Progression {
     public static int[] generateRoundData() {
         Random random = new Random();
         int[] data = new int[DATA_LENGTH];
+
         data[0] = random.nextInt(PROGRESSION_INTERVAL_MAX - PROGRESSION_INTERVAL_MIN + 1)
                 + PROGRESSION_INTERVAL_MIN; //progression length
+        int randomElementNumber = data[0];
         data[1] = random.nextInt(RANDOM_NUMBER_MAX_INTERVAL); //first number
         data[2] = random.nextInt(RANDOM_STEP_INTERVAL_MAX - RANDOM_STEP_INTERVAL_MIN + 1)
                 + RANDOM_STEP_INTERVAL_MIN; //random step
-        data[3] = random.nextInt(data[0]); //random element from 0 to progression length
+        data[3] = random.nextInt(randomElementNumber); //random element from 0 to progression length
         return data;
     }
 
